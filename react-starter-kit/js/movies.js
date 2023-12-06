e4f95a47f891679ffa5fd6e6d5b59d7fa12e2d7a
Albin Lundstrom <albin.lundstrom@elev.ntig.se>
2023-12-06 16:22:53 +0100

    intial comit

---
+++ react-starter-kit/js/movies.js
@@ -0,0 +1,22 @@
+let movies = [
+    {
+        'title':'FNAF MOVIE',
+        'star':5
+    },
+    {
+        'title':'Barbie',
+        'star':4
+    },
+    {
+        'title':'Godfather',
+        'star':1
+    },
+    {
+        'title':'TitenFall',
+        'star':2
+    },
+    {
+        'title':'George',
+        'star':3
+    }
+]
